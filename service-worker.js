a3616cec51bc23f5555b83f76537268ff9f19801
ali-suliman <ali.suliman.alx@gmail.com>
2021-03-10 00:51:29 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1848856840fc07bf33d8453e6cef64fb.js"
+  "/precache-manifest.e2b229a86a9da6467d53df4663f4b1c8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "codic-education"});
